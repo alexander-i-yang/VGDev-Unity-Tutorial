1a1af26c896c41996b19521f360ff7c39fdf2ca7
Alex Yang <alex@yang2k.com>
2023-09-07 16:59:18 -0400

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
 
     // Start is called before the first frame update
     void Start() {
-        
+        HideAndLockCursor();
     }
     
     // Update is called once per frame
